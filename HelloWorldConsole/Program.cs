f654ee176e003ba041a5c54c03fa2b5e7ccda9c7
Jorge Villarreal <jorge_villarreal@epam.com>
2022-10-03 16:03:26 -0500

    task one, create project

---
+++ HelloWorldConsole/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HelloWorldConsole
+{
+
+    public static class Program
+    {
+        static string username = string.Empty;
+
+        static void Main(string[] args)
+        {
+            bool isValidUsername;
+
+            do
+            {
+                Console.Clear();
+                isValidUsername = AskForUsername();
+            } while (!isValidUsername);
+
+            Console.WriteLine($"Hello, {username}!");
+            Console.WriteLine("Type any key to quit");
+            Console.ReadKey();
+        }
+
+        static bool AskForUsername()
+        {
+            Console.WriteLine("Please type your username");
+            username = Console.ReadLine();
+            return !string.IsNullOrEmpty(username);
+        }
+    }
+}
