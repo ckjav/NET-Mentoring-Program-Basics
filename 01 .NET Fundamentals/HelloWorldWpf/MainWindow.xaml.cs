43209f85412cd4f097d607675ba5a8c2d4dd4635
Jorge Villarreal <jorge_villarreal@epam.com>
2022-10-03 16:32:53 -0500

    task two, add library for wpf project

--- 01 .NET Fundamentals/HelloWorldWpf/MainWindow.xaml.cs
+++ 01 .NET Fundamentals/HelloWorldWpf/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Logic;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace HelloWorldWpf
 {
@@ -34,7 +22,7 @@ namespace HelloWorldWpf
             }
             else
             {
-                MessageBox.Show($"Hello, { username}");
+                MessageBox.Show(FormatterMessage.HelloWorld(username));
             }
         }
     }
