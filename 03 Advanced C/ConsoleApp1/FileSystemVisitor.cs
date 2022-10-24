b22301c9ff0e2966a267ae55fc8755c5b9c32988
Jorge Villarreal <jorge_villarreal@epam.com>
2022-10-24 15:18:37 -0500

    task 1

---
+++ 03 Advanced C/ConsoleApp1/FileSystemVisitor.cs
@@ -0,0 +1,39 @@
+﻿namespace ConsoleApp1;
+
+internal class FileSystemVisitor
+{
+    private readonly List<string> Folders;
+    private readonly List<string> Files;
+
+    public FileSystemVisitor(string path)
+    {
+        Folders = Directory.GetDirectories(path).ToList();
+        Files = Directory.GetFiles(path).ToList();
+    }
+
+    public FileSystemVisitor(string path, Func<IEnumerable<string>, IEnumerable<string>> filterFunc) : this(path)
+    {
+        Files = filterFunc(Files).ToList();
+        Folders = filterFunc(Folders).ToList();
+    }
+
+    public IEnumerable<string> GetFolders()
+    {
+        int index = 0;
+        while (index < Folders.Count)
+        {
+            yield return Folders[index];
+            index++;
+        }
+    }
+
+    public IEnumerable<string> GetFiles()
+    {
+        int index = 0;
+        while (index < Files.Count)
+        {
+            yield return Files[index];
+            index++;
+        }
+    }
+}
---
+++ 03 Advanced C/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+var path = string.Empty;
+
+do
+{
+    Console.WriteLine("Please type the path that you want to evaluate its folders and files");
+    path = Console.ReadLine();
+} while (string.IsNullOrEmpty(path));
+
+var fileSystemVisitor = new FileSystemVisitor(path);
+
+Console.WriteLine("Path contains the next folders: ");
+foreach (var folder in fileSystemVisitor.GetFolders())
+{
+    Console.WriteLine(folder);
+}
+Console.WriteLine("Path contains the next files: ");
+foreach (var file in fileSystemVisitor.GetFiles())
+{
+    Console.WriteLine(file);
+}
+
+var fileSystemVisitorWithCarrot = new FileSystemVisitor(path, OnlyElementsWithCarrot);
+Console.WriteLine("Path contains the next folders that contains 'carrot': ");
+foreach (var folder in fileSystemVisitorWithCarrot.GetFolders())
+{
+    Console.WriteLine(folder);
+}
+Console.WriteLine("Path contains the next files that contains 'carrot': ");
+foreach (var file in fileSystemVisitorWithCarrot.GetFiles())
+{
+    Console.WriteLine(file);
+}
+
+IEnumerable<string> OnlyElementsWithCarrot(IEnumerable<string> files)
+{
+    return files.Where(f => f.Contains("carrot", StringComparison.InvariantCultureIgnoreCase));
+}
