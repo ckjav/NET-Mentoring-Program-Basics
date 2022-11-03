2e13d1b0dd57afd39f85ed58cbc2a8797fd52d6b
Jorge Villarreal <jorge_villarreal@epam.com>
2022-11-03 00:13:39 -0600

    task 2, create parser for unit tests

--- 04 Exception handling/Task2/NumberParser.cs
+++ 04 Exception handling/Task2/NumberParser.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace Task2
 {
@@ -6,7 +7,35 @@ namespace Task2
     {
         public int Parse(string stringValue)
         {
-            throw new NotImplementedException();
+            if (stringValue == null) throw new ArgumentNullException("stringValue");
+
+            stringValue = stringValue.Trim();
+
+            Regex regex = new Regex(@"^[+-]?\d+$");
+            if (!regex.IsMatch(stringValue)) throw new FormatException("");
+            
+            int result = 0;
+            int multiplyBy = 1;
+
+            if (stringValue == "-2147483648") return int.MinValue;
+            else if (stringValue == "2147483647") return int.MaxValue;
+            else if (stringValue == "0" || stringValue == "-0" || stringValue == "+0") return 0;
+
+            
+            for (int index = stringValue.Length - 1; index >= 0; --index)
+            {
+                var numberAtPosition = (int)stringValue[index] - 48;
+
+                if (numberAtPosition == -3) result *= -1;
+                else if (numberAtPosition == -5) continue;
+                else result += numberAtPosition * multiplyBy;
+
+                multiplyBy *= 10;
+            }
+
+            if (!stringValue.Contains(Math.Abs(result).ToString()))  throw new OverflowException();
+
+            return result;
         }
     }
 }
