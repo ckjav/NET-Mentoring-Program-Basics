01996539a961bffff670dce2bb231697a24d9f14
Jorge Villarreal <jorge_villarreal@epam.com>
2022-11-02 22:47:03 -0600

    task 1, read each line

--- 04 Exception handling/Task1/Program.cs
+++ 04 Exception handling/Task1/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace Task1
+﻿using System;
+using static System.Console;
+
+namespace Task1
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
-            // TODO: Implement the task here.
+            ReadStringLines();
+        }
+
+        private static void ReadStringLines()
+        {
+            while (true)
+            {
+                WriteLine("Hello! Please type anything and press enter.");
+                var input = ReadLine();
+
+                try
+                {
+                    if (string.IsNullOrEmpty(input)) throw new ArgumentException("");
+
+                    WriteLine($"First char of your previous input was '{input[0]}'");
+                }
+                catch (ArgumentException)
+                {
+                    WriteLine("Previous input was an empty string.");
+                }
+                catch (Exception ex)
+                {
+                    WriteLine("Unexpected exception, details: " + ex.Message);
+                }
+                finally
+                {
+                    WriteLine("Press any key to continue and repeat input.\n");
+                }
+            }
         }
     }
 }
