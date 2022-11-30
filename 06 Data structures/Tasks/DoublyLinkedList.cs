3dbb4daecd61f88dec620b2c6687d63a5fa76b72
Jorge Villarreal <jorge_villarreal@epam.com>
2022-11-30 00:50:10 -0600

    task 1

--- 06 Data structures/Tasks/DoublyLinkedList.cs
+++ 06 Data structures/Tasks/DoublyLinkedList.cs
@@ -7,41 +7,150 @@ namespace Tasks
 {
     public class DoublyLinkedList<T> : IDoublyLinkedList<T>
     {
-        public int Length => throw new NotImplementedException();
+        private int _index = -1;
+        private Node<T> _head;
+
+        public int Length => _index + 1;
 
         public void Add(T e)
         {
-            throw new NotImplementedException();
+            if (_head == null)
+            {
+                _head = new Node<T>(e);
+            }
+            else
+            {
+                var current = _head;
+                
+                for (var i = 0; i < _index; ++i)
+                {
+                    current = _head.Next;
+                }
+
+                current.Next = new Node<T>(e);
+            }
+            ++_index;
         }
 
         public void AddAt(int index, T e)
         {
-            throw new NotImplementedException();
+            if (index < 0 || index > _index + 1) throw new IndexOutOfRangeException();
+
+            var current = _head;
+            for (var i = 0; i < index - 1; ++i)
+            {
+                current = current.Next;
+            }
+
+            Node<T> temp = new Node<T>(e);
+            temp.Next = current.Next;
+            current.Next = temp;
+
+            if (index == 0)
+            {
+                _head = temp;
+            }
+
+            ++_index;
         }
 
         public T ElementAt(int index)
         {
-            throw new NotImplementedException();
+            ValidateIndexForRemoval(index);
+
+            Node<T> current = _head;
+            for (var i = 0; i < index; ++i)
+            {
+                current = current.Next;
+            }
+
+            return current.Data;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            Node<T> current = _head;
+            while(current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
         }
 
         public void Remove(T item)
         {
-            throw new NotImplementedException();
+            ValidateIndexForRemoval();
+
+            var current = _head;
+            var previous = (Node<T>)null;
+
+            while (current != null)
+            {
+                if (current.Data.Equals(item))
+                {
+                    if(previous == null)
+                    {
+                        _head = current.Next;
+                    }
+                    else
+                    {
+                    previous.Next = current.Next;
+
+                    }
+                    --_index;
+                    return;
+                }
+
+                previous = current;
+                current = current.Next;
+            }
         }
 
         public T RemoveAt(int index)
         {
-            throw new NotImplementedException();
+            ValidateIndexForRemoval(index);
+
+            var current = _head;
+            var previous = (Node<T>)null;
+            for (var i = 0; i < index; ++i)
+            {
+                previous = current;
+                current = current.Next;
+            }
+
+            --_index;
+            previous.Next = current.Next;
+
+            return current.Data;
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new NotImplementedException();
+            return new NodeIterator<T>(this._head);
+        }
+
+        private void ValidateIndexForRemoval()
+        {
+            if(_head == null) throw new IndexOutOfRangeException();
+        }
+
+        private void ValidateIndexForRemoval(int index)
+        {
+            ValidateIndexForRemoval();
+            if (index < 0 || index > _index) throw new IndexOutOfRangeException();
+        }
+    }
+
+    public class Node<T>
+    {
+        public T Data { get; set; }
+
+        public Node<T> Next { get; set; }
+
+        public Node(T data)
+        {
+            Data = data;
+            Next = null;
         }
     }
 }
---
+++ 06 Data structures/Tasks/LinkedListIterator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+
+namespace Tasks
+{
+    public class NodeIterator<T> : IEnumerator
+    {
+        private readonly Node<T> _head;
+        private Node<T> _current;
+
+        public NodeIterator(Node<T> head)
+        {
+            _head = head;
+        }
+
+        public object Current => _current.Data;
+
+        public bool MoveNext()
+        {
+            if (_current == null)
+            {
+                _current = _head;
+            }
+            else
+            {
+                _current = _current.Next;
+            }
+
+            return _current != null;
+        }
+
+        public void Reset()
+        {
+            _current = _head;
+        }
+    }
+}
