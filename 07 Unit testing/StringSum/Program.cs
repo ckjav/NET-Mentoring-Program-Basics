4cb109a4231ccf05cd0ec5b1d0244fc5ab76557f
Jorge Villarreal <jorge_villarreal@epam.com>
2022-12-05 08:20:43 -0600

    add base files for StringSum

---
+++ 07 Unit testing/StringSum/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UnitTesting.StringSum;
+
+public class Program
+{
+    public static void Main()
+    {
+        var inputNumber1 = Console.ReadLine();
+        var inputNumber2 = Console.ReadLine();
+    }
+
+    
+}
