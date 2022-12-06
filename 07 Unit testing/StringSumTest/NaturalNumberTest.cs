6d5a6c5c2a5126fe09c3c7176e2c260aa0dedb1e
Jorge Villarreal <jorge_villarreal@epam.com>
2022-12-05 20:24:48 -0600

    add unit test for natural number

--- 07 Unit testing/StringSumTest/NaturalNumberTest.cs
+++ 07 Unit testing/StringSumTest/NaturalNumberTest.cs
@@ -50,4 +50,14 @@ public class NaturalNumberTest
 
         Assert.Equal(0L, response);
     }
+
+    [Fact]
+    public void Parse_WhenReceivesNumber_ShoulReturnNumber()
+    {
+        var input = "2812";
+
+        var response = _naturalNumberSUT.Parse(input);
+
+        Assert.Equal(2812, response);
+    }
 }
