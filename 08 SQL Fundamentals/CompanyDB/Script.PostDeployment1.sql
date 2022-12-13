d822ecb4a153a44e65a2f824a4660fff8400cbd3
Jorge Villarreal <jorge_villarreal@epam.com>
2022-12-12 22:29:37 -0600

    add script deploy

---
+++ 08 SQL Fundamentals/CompanyDB/Script.PostDeployment1.sql
@@ -0,0 +1,9 @@
+﻿MERGE INTO Person AS TARGET
+USING (VALUES
+	(1, 'Andrew', 'White')
+)
+AS SOURCE([ID], FirstName, LastName)
+ON TARGET.[ID] = SOURCE.[Id]
+WHEN NOT MATCHED BY TARGET THEN
+INSERT (FirstName, LastName)
+VALUES (FirstName, LastName);
