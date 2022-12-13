e056d654c58bffed28fb2aff2dc2fd3437874fa2
Jorge Villarreal <jorge_villarreal@epam.com>
2022-12-13 07:55:11 -0600

    remove company join and use employee company name

--- 08 SQL Fundamentals/CompanyDB/EmployeeInfo.sql
+++ 08 SQL Fundamentals/CompanyDB/EmployeeInfo.sql
@@ -3,8 +3,7 @@
 		e.Id AS EmployeeId,
 		COALESCE(e.EmployeeName, p.FirstName + ' ' + p.LastName) AS EmployeeFullName,
 		a.ZipCode + '_' + a.State + ',' + a.City + '-' + a.Street EmployeeFullAddress,
-		c.Name + '(' + e.Position + ')' EmployeeCompanyInfo
+		e.CompanyName + '(' + e.Position + ')' EmployeeCompanyInfo
 	FROM [dbo].[Employee] e
 	JOIN [dbo].[Person] p ON p.Id = e.PersonId
 	JOIN [dbo].[Address] a ON a.Id = e.AddressId
-	JOIN [dbo].[Company] c ON c.AddressId = a.Id
