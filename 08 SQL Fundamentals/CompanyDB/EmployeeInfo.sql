564bdac86c5d292e06cb82805173ddcac32a11bc
Jorge Villarreal <jorge_villarreal@epam.com>
2022-12-12 22:29:47 -0600

    add view

---
+++ 08 SQL Fundamentals/CompanyDB/EmployeeInfo.sql
@@ -0,0 +1,10 @@
+﻿CREATE VIEW [dbo].[EmployeeInfo]
+	AS SELECT
+		e.Id AS EmployeeId,
+		COALESCE(e.EmployeeName, p.FirstName + ' ' + p.LastName) AS EmployeeFullName,
+		a.ZipCode + '_' + a.State + ',' + a.City + '-' + a.Street EmployeeFullAddress,
+		c.Name + '(' + e.Position + ')' EmployeeCompanyInfo
+	FROM [dbo].[Employee] e
+	JOIN [dbo].[Person] p ON p.Id = e.PersonId
+	JOIN [dbo].[Address] a ON a.Id = e.AddressId
+	JOIN [dbo].[Company] c ON c.AddressId = a.Id
