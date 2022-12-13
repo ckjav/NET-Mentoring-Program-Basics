1c2b9821b05d1c1b9e69f86e327753b82b5676ae
Jorge Villarreal <jorge_villarreal@epam.com>
2022-12-12 22:29:16 -0600

    add tables

---
+++ 08 SQL Fundamentals/CompanyDB/Employee.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Employee]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	AddressId INT NOT NULL FOREIGN KEY REFERENCES [dbo].[Address](Id),
+	PersonId INT NOT NULL FOREIGN KEY REFERENCES [dbo].[Person](Id),
+	CompanyName NVARCHAR(20),
+	Position NVARCHAR(30),
+	EmployeeName NVARCHAR(100)
+)
