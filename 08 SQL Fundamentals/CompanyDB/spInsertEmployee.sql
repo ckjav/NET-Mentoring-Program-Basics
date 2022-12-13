6d891a48799c37418ba3850b8c7f67f60a7c1a15
Jorge Villarreal <jorge_villarreal@epam.com>
2022-12-12 22:29:54 -0600

    add stored procedure

---
+++ 08 SQL Fundamentals/CompanyDB/spInsertEmployee.sql
@@ -0,0 +1,33 @@
+﻿CREATE PROCEDURE [dbo].[spInsertEmployee]
+	@employeeName NVARCHAR(100) = NULL,
+	@firstName NVARCHAR(50) = NULL,
+	@lastName NVARCHAR(50) = NULL,
+	@companyName NVARCHAR(20),
+	@position NVARCHAR(30) = NULL,
+	@street NVARCHAR(50),
+	@city NVARCHAR(20) = NULL,
+	@state NVARCHAR(50) = NULL,
+	@zipCode NVARCHAR(50) = NULL
+AS
+
+	IF @firstName = NULL OR TRIM(@firstName) = ''
+	BEGIN
+		PRINT 'First name must not be an empty string.'
+		RETURN
+	END
+
+	INSERT INTO [dbo].[Person] (FirstName, LastName)
+	VALUES (@firstName, @lastName);
+	DECLARE @personId INT = @@IDENTITY
+
+	INSERT INTO [dbo].[Address] (Street, City, State, ZipCode)
+	VALUES (@street, @city, @state, @zipCode);
+	DECLARE @addressId INT = @@IDENTITY
+
+	INSERT INTO [dbo].[Employee] (AddressId, PersonId, CompanyName, Position, EmployeeName)
+	VALUES (@addressId, @personId, @companyName, @position, @employeeName);
+
+	INSERT INTO [dbo].[Company] (Name, AddressId)
+	VALUES (@companyName, @addressId);
+
+RETURN 0
