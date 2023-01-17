1604a04281d24e5ad294f59b26ddcf57c3ebb1e8
Jorge Villarreal <jorge_villarreal@epam.com>
2023-01-16 23:41:23 -0600

    add json serializer console app

---
+++ 10 Serialization/JsonSerializerConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿using BaseLibrary;
+
+namespace JsonSerializerConsole;
+
+public class Program : Menu
+{
+    static readonly string filename = "department.json";
+
+    public static void Main()
+    {
+        var serializer = new Serializer(filename);
+        DisplayOptionsForSerialization(serializer, filename);
+    }
+}
---
+++ 10 Serialization/JsonSerializerConsole/Serializer.cs
@@ -0,0 +1,62 @@
+﻿using BaseLibrary;
+using System.Text.Json;
+
+namespace JsonSerializerConsole;
+
+public class Serializer : Menu, ISerializerApp
+{
+    private readonly string _filename;
+
+    public Serializer(string filename)
+    {
+        _filename = filename;
+    }
+
+    public void Serialize()
+    {
+        var department = Seeder.GetDepartment();
+
+        DisplayEntryMessage();
+        DisplayDepartmentInfo(department);
+
+        try
+        {
+            var jsonString = JsonSerializer.Serialize(department);
+            //using Stream stream = new FileStream(_filename, FileMode.Create, FileAccess.Write, FileShare.None);
+            File.WriteAllText(_filename, jsonString);
+            //stream.Close();
+            DisplayFileCreationMessage(_filename);
+        }
+        catch
+        {
+            DisplayWritingFileErrorMessage();
+            throw;
+        }
+    }
+
+    public void Deserialize()
+    {
+        var currentDirectory = Directory.GetCurrentDirectory();
+        if (!ValidateFilePath(currentDirectory, _filename))
+        {
+            return;
+        }
+
+        Console.WriteLine($"Deserializing file {_filename}");
+
+        try
+        {
+            using Stream stream = new FileStream(_filename, FileMode.Open, FileAccess.Read, FileShare.None);
+            var department = JsonSerializer.Deserialize<Department>(stream);
+            stream.Close();
+
+            Console.WriteLine("File had been deserialized.");
+            DisplayDepartmentInfo(department);
+        }
+        catch
+        {
+            DisplayReadingFileErrorMessage(_filename);
+            throw;
+        }
+    }
+}
